a4efb5b3a852be99805c12735a15778f44ec81c9
Yoseph Ayalew <yosefaylew56@gmail.com>
2025-05-29 18:50:41 -0700

    last

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -96,7 +96,7 @@ const PortfolioPage = () => {
                 </defs>
                  <text fill="#000">
                   <textPath xlinkHref="#circlePath" className="text-xl">
-                    Front-end Developer and UI Designer
+                    Front-end Developer 
                   </textPath>
                 </text>
               </motion.svg>
