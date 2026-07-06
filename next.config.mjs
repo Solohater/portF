93ea8134b8d8f5daddcb3bc16f3bab9f2429d6b4
yosephh <yosefayalew56@gmail.com>
2026-07-06 15:50:43 +0300

    Add static export config for Render deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"images.pexels.com",
-            }
-        ]
-    }
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
